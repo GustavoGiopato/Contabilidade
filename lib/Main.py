a187e13740c34b006b4390ff0d0892562caa5361
Gustavo Giopato Manfredi <gustavogimanfredi51@gmail.com>
2023-07-03 19:58:35 -0300

    funções de conexão com banco arrumadas por funçã

--- lib/Main.py
+++ lib/Main.py
@@ -1,6 +1,12 @@
-from Db import *
-import tkinter
+from Db import create_connection, obter_livro_diario
 
-livro_diario = obter_livro_diario()
+connection, cursor = create_connection()
+
+livro_diario = obter_livro_diario(connection, cursor)
+
+
 for registro in livro_diario:
     print(registro)
+
+cursor.close()
+connection.close()
